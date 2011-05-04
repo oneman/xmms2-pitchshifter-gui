c112564654d5e381428412a8e82df6f74e7c2662
David Richards <rawdod@gmail.com>
2011-05-03 21:19:14 -0400

    better defaults and noobie help

--- xmms2-pitchshifter.rb
+++ xmms2-pitchshifter.rb
@@ -23,19 +23,29 @@ end
 xc.add_to_glib_mainloop
 
 window = Gtk::Window.new(Gtk::Window::TOPLEVEL)
-window.set_title  "XMMS2 Pitchshifter"
+window.set_title "XMMS2 Pitchshifter"
 window.border_width = 20
 window.signal_connect('delete_event') { Gtk.main_quit }
-window.set_size_request(250, -1)
+window.set_size_request(450, -1)
 float = Gtk::HScale.new(lowest_value, highest_value, 0.2)
 float.value_pos = Gtk::POS_RIGHT
 vbox = Gtk::VBox.new(true, 5)
-vbox.pack_start(float,   false, true, 0)
+vbox.pack_start(float, false, true, 0)
 
 res = xc.config_get_value("pitch.pitch")
 res.notifier { |notifier| float.value = notifier.to_f }
 
- 
+res = xc.config_get_value("pitch.enabled")
+res.notifier { |notifier| 
+
+	if notifier.to_i <= 0
+		puts "Pitch plugin is not enabled..."
+		puts "To enable it: nyxmms2 server config pitch.enabled 1"
+		puts "And: nyxmms2 server config effect.order.0 pitch"
+	end
+	
+}
+
 float.signal_connect('value-changed') {
 
 	pitch = float.value.to_s.split(".").first
